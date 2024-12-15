a6a425accb16b1832d98270cd6d5a8dadec1e34f
Kiran Khaire <khairekiran2@gmail.com>
2024-12-15 22:34:19 +0530

    project sequence changed

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -10,9 +10,9 @@ import { CommonModule } from '@angular/common';
 })
 export class PortfolioComponent {
   projects = [
-    { name: 'Air Suspension Hose Leak Detection', description: 'Windows-based industrial automation system for leak detection of automobile parts'},
     { name: 'IoT Device Management System', description: 'Client-server architecture for managing IoT devices', link: 'https://github.com/WasterK/KWS-IoT-Backend',tryLink: 'https://kws-pmd-web.onrender.com', show: true },
+    { name: 'GenAI: Cold Email Generator', description: 'AI-powered cold email generator which matches porfolios with job descriptions', link: 'https://github.com/WasterK/GenAI-cold-email-generator', tryLink: 'https://genai-cold-email-generator.onrender.com', show: true},
     { name: 'Digi Home', description: 'Web-based application for monitoring temperaton, humidity', link: 'https://github.com/WasterK/tc-workshop-digihome', show: true },
-    { name: 'GenAI: Cold Email Generator', description: 'AI-powered cold email generator which matches porfolios with job descriptions', link: 'https://github.com/WasterK/GenAI-cold-email-generator', tryLink: 'https://genai-cold-email-generator.onrender.com', show: true}
+    { name: 'Air Suspension Hose Leak Detection', description: 'Windows-based industrial automation system for leak detection of automobile parts'}
   ];
 }
